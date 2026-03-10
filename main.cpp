5a47a71b3396a84986761fadab0961a333bd7e0a
Danila Zubar <dan6868vv@gmail.com>
2026-03-10 16:26:41 +0300

    Smooth needle rotate deepseek code version 1.16

--- main.cpp
+++ main.cpp
@@ -144,7 +144,7 @@ int main() {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         BeginMode3D(camera);
-//#ifdef __unix__
+#ifdef __unix__
 
         getJsonByPipe(jsonMapTarget);
 
