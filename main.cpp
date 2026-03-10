614a2fdd6a64a75abb64659cfba2721a1ad42dd0
Danila Zubar <dan6868vv@gmail.com>
2026-03-10 15:11:59 +0300

    Smooth needle rotate deepseek code version 1.2

--- main.cpp
+++ main.cpp
@@ -71,7 +71,7 @@ void getJsonByPipe(std::unordered_map<std::string, std::string> &jsonMap) {
                 std::string key = item.substr(0, pos);
                 if(key == "id" && item.substr(pos + 1)!=__id)
                     return;
-                jsonMap[key] = item.substr(pos + 1);
+                jsonMap[key] = stof(item.substr(pos + 1));
             }
         }
         break;
@@ -89,67 +89,29 @@ bool importModels(std::unordered_map<std::string, std::string> jsonMap,
         Model model = LoadModel(("/home/andrey/qwer/clock/_models_for_unix/" + id +"/" + i.first+ ".obj").c_str());
         modelMap[i.first] = model;
 #elif defined(_WIN64)
-        Model model = LoadModel(("D:/_root/Job/AeroMash_new/Arrow_Display/_models_for_win/" + id +"/" + i.first + ".obj").c_str());
+        Model model = LoadModel(
+            ("D:/_root/Job/AeroMash_new/Arrow_Display/_models_for_win/" + id + "/" + i.first + ".obj").c_str());
         modelMap[i.first] = model;
 #endif
     }
     return true;
 }
 
-// int main() {
-//     InitWindow(800, 800, "3D Clock");
-//
-// #ifdef _WIN64
-//     Model clock = LoadModel("D:/_root/Job/AeroMash_new/Arrow_Display/_models_for_win/tv-45_frame.obj");
-//     Model needle = LoadModel("D:/_root/Job/AeroMash_new/Arrow_Display/_models_for_win/tv-45_needle.obj");
-// #endif
-//
-//     Camera3D camera = {0};
-//     camera.position = (Vector3){45.0f, 0.0f, 0.0f};
-//     camera.target = (Vector3){0.0f, 0.0f, 0.0f};
-//     camera.up = (Vector3){0.0f, 2.0f, 0.0f};
-//     camera.fovy = 90.0f;
-//
-//     std::unordered_map<std::string, std::string> jsonMap;
-//     std::unordered_map<std::string, Model> modelMap;
-// #ifdef __unix__
-//     getJsonByPipe(jsonMap);
-//     importModels(jsonMap,modelMap);
-// #elif defined(_WIN64)
-//     float angle = 0;
-//     angle += 1;
-// #endif
-//     BeginDrawing();
-//     ClearBackground(RAYWHITE);
-//     BeginMode3D(camera);
-//     while (!WindowShouldClose()) {
-//
-// #ifdef __unix__
-//         getJsonByPipe(jsonMap);
-//         for(auto& it:modelMap) {
-//             it.second.transform = MatrixRotateX(DEG2RAD * stof(jsonMap[it.first]));
-//             DrawModel(it.second, (Vector3){0, 0, 0}, 1.0f, WHITE);
-//         }
-// #endif
-//
-//         EndMode3D();
-//         DrawFPS(10, 10);
-//         EndDrawing();
-//
-//         for (auto i: jsonMap) {
-//             std::cout << i.first << ": " << i.second << std::endl;
-//         }
-//     }
-//
-//     for(auto it:modelMap) {
-//         UnloadModel(it.second);
-//     }
-//
-//     CloseWindow();
-//     return 0;
-// }
+void getDiff(std::unordered_map<std::string, std::string> jsonMapTarget,
+             std::unordered_map<std::string, std::string> jsonMapCurrent,
+             std::unordered_map<std::string, float> &jsonMapDifferent) {
+    for (auto itTarget: jsonMapTarget) {
+        jsonMapDifferent[itTarget.first] = std::stof(itTarget.second) - std::stof(jsonMapCurrent[itTarget.first]);
+    }
+}
+
 int main() {
     InitWindow(800, 800, "3D Clock");
+
+#ifdef _WIN64
+    Model clock = LoadModel("D:/_root/Job/AeroMash_new/Arrow_Display/_models_for_win/tv-45_frame.obj");
+    Model needle = LoadModel("D:/_root/Job/AeroMash_new/Arrow_Display/_models_for_win/tv-45_needle.obj");
+#endif
 
     Camera3D camera = {0};
     camera.position = (Vector3){45.0f, 0.0f, 0.0f};
@@ -157,65 +119,27 @@ int main() {
     camera.up = (Vector3){0.0f, 2.0f, 0.0f};
     camera.fovy = 90.0f;
 
-    std::unordered_map<std::string, std::string> jsonMap;
+    std::unordered_map<std::string, float> jsonMapTarget;
+    std::unordered_map<std::string, float> jsonMapCurrent;
+    std::unordered_map<std::string, float> jsonMapDifferent;
     std::unordered_map<std::string, Model> modelMap;
-    std::unordered_map<std::string, float> currentAngles; // Текущие углы
-    std::unordered_map<std::string, float> targetAngles;  // Целевые углы
-
-    float smoothFactor = 0.15f; // Коэффициент плавности (0-1, где 1 - мгновенно)
-
 #ifdef __unix__
-    getJsonByPipe(jsonMap);
-    importModels(jsonMap, modelMap);
-
-    // Инициализируем углы
-    for(auto& it : modelMap) {
-        currentAngles[it.first] = 0.0f;
-        if (jsonMap.find(it.first) != jsonMap.end()) {
-            targetAngles[it.first] = std::stof(jsonMap[it.first]);
-        } else {
-            targetAngles[it.first] = 0.0f;
-        }
-    }
+    getJsonByPipe(jsonMapTarget);
+    importModels(jsonMapTarget, modelMap);
+    jsonMapCurrent = jsonMapTarget;
+#elif defined(_WIN64)
+    float angle = 0;
+    angle += 1;
 #endif
-
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    BeginMode3D(camera);
     while (!WindowShouldClose()) {
-        float deltaTime = GetFrameTime();
-
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        BeginMode3D(camera);
-
-#ifdef __unix__
-        // Получаем новые данные
-        getJsonByPipe(jsonMap);
-
-        // Обновляем целевые углы
-        for(auto& it : modelMap) {
-            if (jsonMap.find(it.first) != jsonMap.end()) {
-                targetAngles[it.first] = std::stof(jsonMap[it.first]);
-            }
-        }
-
-        // Плавно поворачиваем каждую модель
-        for(auto& it : modelMap) {
-            float& current = currentAngles[it.first];
-            float target = targetAngles[it.first];
-
-            // Находим кратчайший путь
-            float diff = target - current;
-            if (diff > 180.0f) diff -= 360.0f;
-            if (diff < -180.0f) diff += 360.0f;
-
-            // Применяем плавность
-            current += diff * smoothFactor * (deltaTime * 60.0f); // Нормализуем по времени
-
-            // Нормализуем угол
-            if (current >= 360.0f) current -= 360.0f;
-            if (current < 0.0f) current += 360.0f;
-
-            // Применяем поворот
-            it.second.transform = MatrixRotateX(DEG2RAD * current);
+        #ifdef __unix__
+        getJsonByPipe(jsonMapTarget);
+        for (auto &it: modelMap) {
+            it.second.transform =
+                    MatrixRotateX(DEG2RAD * jsonMapCurrent[it.first]);
             DrawModel(it.second, (Vector3){0, 0, 0}, 1.0f, WHITE);
         }
 #endif
@@ -223,8 +147,16 @@ int main() {
         EndMode3D();
         DrawFPS(10, 10);
         EndDrawing();
+
+        for (auto i: jsonMapTarget) {
+            std::cout << i.first << ": " << i.second << std::endl;
+        }
     }
 
-    // Cleanup...
+    for (auto it: modelMap) {
+        UnloadModel(it.second);
+    }
+
+    CloseWindow();
     return 0;
 }
